73bbd92e8b3c5ece47466f08c4e4e116298083ee
biyuhuaping <biyuhuaping@163.com>
2018-01-24 11:36:54 +0800

    添加登录、绑定手机号页面。

---
+++ zhike/zhike/Controllers/welcome/BindingViewController.h
@@ -0,0 +1,13 @@
+//
+//  BindingViewController.h
+//  zhike
+//
+//  Created by 周博 on 2018/1/24.
+//  Copyright © 2018年 zhoubo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BindingViewController : UIViewController
+
+@end
--- zhike/zhike/Controllers/welcome/BindingViewController.m
+++ zhike/zhike/Controllers/welcome/BindingViewController.m
@@ -1,50 +1,39 @@
 //
-//  LoginViewController.m
+//  BindingViewController.m
 //  zhike
 //
-//  Created by 周博 on 2018/1/18.
+//  Created by 周博 on 2018/1/24.
 //  Copyright © 2018年 zhoubo. All rights reserved.
 //
 
-#import "LoginViewController.h"
-#import <UMSocialCore/UMSocialCore.h>
 #import "BindingViewController.h"
 
-@interface LoginViewController ()
+@interface BindingViewController ()
 
 @property (strong, nonatomic) IBOutlet UITextField *textField1;
 @property (strong, nonatomic) IBOutlet UITextField *textField2;
 @property (strong, nonatomic) IBOutlet UIButton *codeButton;
-@property (strong, nonatomic) IBOutlet UIButton *loginButton;
+@property (strong, nonatomic) IBOutlet UIButton *nextButton;
 @property (strong, nonatomic) IBOutlet NSLayoutConstraint *lineHeight;
 
 @end
 
-@implementation LoginViewController
+@implementation BindingViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
-    self.title = @"登录";
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"关闭" style:UIBarButtonItemStylePlain target:self action:@selector(dismissView)];
+    self.title = @"绑定手机号";
     self.edgesForExtendedLayout = UIRectEdgeNone;
-
+    
     self.codeButton.layer.cornerRadius = 4;
-    self.loginButton.layer.cornerRadius = 4;
+    self.nextButton.layer.cornerRadius = 4;
     self.lineHeight.constant = 0.5;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-}
-
-- (void)dismissView{
-    [self dismissViewControllerAnimated:YES completion:nil];
-//    DBLOG(@"%u",self.tabBarController.selectedIndex);
-//    if (self.tabBarController.selectedIndex == 2) {
-//        [self.tabBarController setSelectedIndex:0];
-//    }
 }
 
 //获取验证码
@@ -56,10 +45,17 @@
     [self setTheCountdownButton:sender startWithTime:10 title:@"获取验证码" countDownTitle:@"s" mainColor:mainColor countColor:countColor];
 }
 
-//登录
-- (IBAction)loginButtonTap:(id)sender {
-    BindingViewController *bindView = [[BindingViewController alloc]initWithNibName:@"BindingViewController" bundle:nil];
-    [self.navigationController pushViewController:bindView animated:YES];
+//下一步
+- (IBAction)nextButtonTap:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"绑定成功" message:@"zhoubo" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+        DBLog(@"点击了取消");
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+        DBLog(@"点击了确定");
+        [self dismissViewControllerAnimated:YES completion:nil];
+    }]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 #pragma mark - button倒计时
@@ -91,52 +87,6 @@
         }
     });
     dispatch_resume(_timer);
-}
-
-#pragma mark - 第三方登录
-//微信登录
-- (IBAction)wechatButtonTap:(id)sender {
-    [self getUserInfoForPlatform:1];
-}
-
-//QQ登录
-- (IBAction)QQButtonTap:(id)sender {
-    [self getUserInfoForPlatform:5];
-}
-
-//新浪微博登录
-- (IBAction)SinaButtonTap:(id)sender {
-    [self getUserInfoForPlatform:0];
-}
-
-//淘宝登录
-- (IBAction)taobaoButtonTap:(id)sender {
-}
-
-- (void)getUserInfoForPlatform:(UMSocialPlatformType)platformType{
-    [[UMSocialManager defaultManager] getUserInfoWithPlatform:platformType currentViewController:self completion:^(id result, NSError *error) {
-        UMSocialUserInfoResponse *resp = result;
-        // 第三方登录数据(为空表示平台未提供)
-        // 授权数据
-        NSLog(@" uid: %@", resp.uid);
-        NSLog(@" openid: %@", resp.openid);
-        NSLog(@" accessToken: %@", resp.accessToken);
-        NSLog(@" refreshToken: %@", resp.refreshToken);
-        NSLog(@" expiration: %@", resp.expiration);
-        
-        // 用户数据
-        NSLog(@" name: %@", resp.name);
-        NSLog(@" iconurl: %@", resp.iconurl);
-        NSLog(@" gender: %@", resp.unionGender);
-        
-        // 第三方平台SDK原始数据
-        NSLog(@" originalResponse: %@", resp.originalResponse);
-    }];
-}
-
-- (void)dealloc{
-//    self.textField1 = nil;
-//    self.textField2 = nil;
 }
 
 @end
